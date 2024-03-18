d09d3bc551eb8bb1e239084ee413de26278d8996
daesvi <villegasdaniel4321@gmail.com>
2024-03-18 10:28:12 -0500

    Fixes and optimization to add technologies

--- src/main/java/com/example/microserviciobootcamp/adapters/driving/http/controller/TechnologyRestControllerAdapter.java
+++ src/main/java/com/example/microserviciobootcamp/adapters/driving/http/controller/TechnologyRestControllerAdapter.java
@@ -1,6 +1,7 @@
 package com.example.microserviciobootcamp.adapters.driving.http.controller;
 
 import com.example.microserviciobootcamp.adapters.driving.http.dto.request.AddTechnologyRequest;
+import com.example.microserviciobootcamp.adapters.driving.http.dto.response.TechnologyResponse;
 import com.example.microserviciobootcamp.adapters.driving.http.mapper.ITechnologyRequestMapper;
 import com.example.microserviciobootcamp.adapters.driving.http.mapper.ITechnologyResponseMapper;
 import com.example.microserviciobootcamp.adapters.driving.http.service.TechnologyHandlerImpl;
@@ -9,10 +10,9 @@ import com.example.microserviciobootcamp.domain.api.ITechnologyServicePort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/technology")
@@ -28,6 +28,13 @@ public class TechnologyRestControllerAdapter {
     public ResponseEntity<ExceptionResponse> addTechnology(@RequestBody AddTechnologyRequest request) {
         technologyHandler.saveTechnology(request);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TechnologyResponse>> getAllTechnologies (@RequestParam(defaultValue = "1") Integer page,
+                                                                        @RequestParam(defaultValue = "10") Integer size,
+                                                                        @RequestParam (required = false) Boolean ascending){
+        return ResponseEntity.ok(technologyResponseMapper.toProductResponseList(technologyServicePort.getAllTechnologies(page, size, ascending)));
     }
 }
 
--- src/main/java/com/example/microserviciobootcamp/adapters/driving/http/service/TechnologyHandlerImpl.java
+++ src/main/java/com/example/microserviciobootcamp/adapters/driving/http/service/TechnologyHandlerImpl.java
@@ -2,11 +2,11 @@ package com.example.microserviciobootcamp.adapters.driving.http.service;
 
 import com.example.microserviciobootcamp.adapters.driving.http.dto.request.AddTechnologyRequest;
 import com.example.microserviciobootcamp.adapters.driving.http.mapper.ITechnologyRequestMapper;
+import com.example.microserviciobootcamp.domain.api.ITechnologyServicePort;
 import com.example.microserviciobootcamp.domain.exception.EmptyFieldException;
 import com.example.microserviciobootcamp.domain.exception.FieldExceedsCharactersException;
 import com.example.microserviciobootcamp.domain.exception.MissingDataException;
 import com.example.microserviciobootcamp.domain.model.Technology;
-import com.example.microserviciobootcamp.domain.spi.ITechnologyPersistencePort;
 import com.example.microserviciobootcamp.domain.util.DomainConstants;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,27 +15,27 @@ import org.springframework.stereotype.Service;
 @Service
 public class TechnologyHandlerImpl implements ITechnologyHandler {
 
-    private final ITechnologyPersistencePort technologyPersistencePort;
     private final ITechnologyRequestMapper technologyRequestMapper;
+    private final ITechnologyServicePort technologyServicePort;
     private static final int MAX_NAME_LENGTH = 50;
     private static final int MAX_DESCRIPTION_LENGTH = 90;
 
 
     @Override
     public void saveTechnology(AddTechnologyRequest request) {
-        if (request.getName() == null) {
-            throw new MissingDataException(DomainConstants.Field.NAME.toString());
-        } else if (request.getDescription() == null) {
-            throw new MissingDataException(DomainConstants.Field.DESCRIPTION.toString());
-        }
         validateTechnology(request);
         Technology technology = technologyRequestMapper.addRequestToTechnology(request);
-        technologyPersistencePort.saveTechnology(technology);
+        technologyServicePort.saveTechnology(technology);
     }
 
     private void validateTechnology(AddTechnologyRequest request) {
         String nameTechnology = request.getName();
         String descriptionTechnology = request.getDescription();
+        if (nameTechnology == null) {
+            throw new MissingDataException(DomainConstants.Field.NAME.toString());
+        } else if (descriptionTechnology == null) {
+            throw new MissingDataException(DomainConstants.Field.DESCRIPTION.toString());
+        }
         if (nameTechnology.isEmpty()) {
             throw new EmptyFieldException(DomainConstants.Field.NAME.toString());
         }else if (nameTechnology.length()>50){
@@ -46,9 +46,5 @@ public class TechnologyHandlerImpl implements ITechnologyHandler {
         } else if (descriptionTechnology.length()>90){
             throw new FieldExceedsCharactersException(DomainConstants.Field.DESCRIPTION.toString());
         }
-    }
-
-    private Technology mapRequestToTechnology(AddTechnologyRequest request) {
-        return technologyRequestMapper.addRequestToTechnology(request);
     }
 }
