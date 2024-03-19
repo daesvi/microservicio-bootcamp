412ddbc53c78d7f79d4faab23e1c7f6d82e6bd21
daesvi <villegasdaniel4321@gmail.com>
2024-03-19 10:06:05 -0500

    Unnecessary imports are eliminated

--- src/main/java/com/example/microserviciobootcamp/domain/api/ITechnologyServicePort.java
+++ src/main/java/com/example/microserviciobootcamp/domain/api/ITechnologyServicePort.java
@@ -3,7 +3,6 @@ package com.example.microserviciobootcamp.domain.api;
 import com.example.microserviciobootcamp.domain.model.Technology;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ITechnologyServicePort {
     Technology saveTechnology (Technology technology);
